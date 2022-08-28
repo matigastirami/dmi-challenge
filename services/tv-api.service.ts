7b586d824ad797dc3be1c566950185a92a4225d7
matigastirami <matigastirami@gmail.com>
2022-08-28 20:34:00 -0300

    fix: using env variables

--- services/tv-api.service.ts
+++ services/tv-api.service.ts
@@ -1,13 +1,11 @@
 export default class TvApiService {
-    private static readonly API_URL = 'https://api.tvmaze.com';
-
     static searchShows(query: string) {
-        const baseUrl = new URL(`${TvApiService.API_URL}/search/shows`);
+        const baseUrl = new URL(`${process.env.NEXT_PUBLIC_TVMAZE_API_URL}/search/shows`);
         baseUrl.searchParams.append('q', query);
         return fetch(baseUrl);
     }
 
     static getById(id: any) {
-        return fetch(`${TvApiService.API_URL}/shows/${id}`);
+        return fetch(`${process.env.NEXT_PUBLIC_TVMAZE_API_URL}/shows/${id}`);
     }
 }
